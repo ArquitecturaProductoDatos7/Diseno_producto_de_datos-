704ba4b04a820ad31f65d10e3bd1ac11bb8e9dbf
Bcjg23 <54872638+Bcjg23@users.noreply.github.com>
2020-03-03 00:39:27 -0600

    Add files via upload

---
+++ script/etl_pipeline.py
@@ -0,0 +1,155 @@
+import json
+import os
+import luigi.contrib.s3
+import datetime
+import boto3
+
+import luigi
+import pandas as pd
+
+import extrae
+
+
+DATAURL = "https://datos.cdmx.gob.mx/api/records/1.0/search/?dataset=incidentes-viales-c5"
+
+
+
+class extraeInfoPrimeraVez(luigi.Task):
+    """
+    Extrae toda la informacion: desde el inicio (1-Ene-2014) hasta 2 meses antes de la fecha actual
+    """
+    
+    #Parametros para la ruta en S3
+    bucket = "dpa20"
+    root_path = "incidentes_viales_CDMX"
+    etl_path = "raw"
+    year = 0
+    month = 0
+    fname = ""
+            
+    def run(self):
+        #Parametros de los datos
+        #DATE_START = datetime.date(2014,1,1)
+        DATE_START = datetime.date(2019,8,1)
+        date_today = datetime.date.today()
+        date_end = datetime.date(date_today.year, date_today.month - 2, 31)
+        
+        #periodo de fechas mensuales
+        dates = pd.period_range(start=str(DATE_START), end=str(date_end), freq='M')
+        
+        for date in dates:
+            self.year = date.year
+            self.month = date.month
+            
+            #nombre del archivo
+            self.fname = "incidentes_viales_" + str(self.month) + str(self.year)
+            
+            #hacemos el requerimiento para un chunk del los registros
+            response = extrae.peticion_api_info_mensual(DATAURL, self.month, self.year)
+            
+            #guardamos la info en un S3
+            ses = boto3.session.Session(profile_name='default', region_name='us-east-1')
+            s3_resource = ses.resource('s3')
+            
+            obj=s3_resource.Bucket(self.bucket)
+            
+            with self.output().open('w') as self.fname:
+                self.fname.write(response)
+    
+    
+    def output(self):
+        output_path = "s3://{}/{}/{}/YEAR={}/MONTH={}/{}.csv".\
+        format(self.bucket, 
+               self.root_path,
+               self.etl_path,
+               self.year,
+               self.month,
+               self.fname
+              )
+        
+        return luigi.contrib.s3.S3Target(path=output_path)
+    
+
+
+
+    
+
+
+class extraeInfoMensual(luigi.Task):
+    """
+    Esta tarea estrae la informacion mensual
+    """
+    
+    #Parametros para la ruta en S3
+    bucket = "dpa20"
+    root_path = "incidentes_viales_CDMX"
+    etl_path = "raw"
+    year = 0
+    month = 0
+    fname = "incidentes_viales_" + str(month) + str(year)
+    
+    def run(self):
+        #hacemos el requerimiento para un chunk del los registros
+        response = extrae.peticion_api_info_mensual(DATAURL, self.month, self.year)
+    
+        #guardamos la info en un S3
+        ses = boto3.session.Session(profile_name='default', region_name='us-east-2')
+        s3_resource = ses.resource('s3')
+              
+        obj=s3_resource.Bucket(self.bucket)
+        
+        with self.output().open('w') as fout:
+            fout.write(response)
+         
+        
+    def output(self):
+        output_path = "s3://{}/{}/{}/YEAR={}/MONTH={}/{}.csv".\
+        format(self.bucket, 
+               self.root_path,
+               self.etl_path,
+               self.year,
+               self.month,
+               self.fname
+              )
+        
+        return luigi.contrib.s3.S3Target(path=output_path)
+    
+     
+        
+        
+        
+
+        
+class extraeChunkAPIaS3(luigi.Task):
+    """
+    Fetches a chunk of information from API to a S3 bucket.
+    """
+    #Parametros para la ruta en S3
+    bucket = luigi.Parameter()
+    root_path = luigi.Parameter()
+    year = luigi.Parameter()
+    fname = luigi.Parameter()
+        
+    def run(self):
+        #hacemos el requerimiento para un chunk del los registros
+        response = extrae.peticion_api_info_mensual(DATAURL, 1, 2014)
+    
+        #guardamos la info en un S3
+        ses = boto3.session.Session(profile_name='default', region_name='us-east-1')
+        s3_resource = ses.resource('s3')
+              
+        obj=s3_resource.Bucket(self.bucket)
+        
+        with self.output().open('w') as fout:
+            fout.write(response)
+         
+        
+    def output(self):
+        output_path = "s3://{}/{}/YEAR={}/{}.csv".\
+        format(self.bucket, 
+               self.root_path,
+               self.year,
+               self.fname
+              )
+        
+        return luigi.contrib.s3.S3Target(path=output_path)
