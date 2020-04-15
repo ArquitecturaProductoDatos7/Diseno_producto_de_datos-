2b54762d5dbad5d9bb4a3307de40c7858090eb9f
Bcjg23 <54872638+Bcjg23@users.noreply.github.com>
2020-04-15 18:17:03 -0500

    Update funciones_rds.py

--- script/luigi_files/funciones_rds.py
+++ script/luigi_files/funciones_rds.py
@@ -33,8 +33,11 @@ def create_db_instance(db_instance_id, db_name, db_user, db_pass, subnet_gp, sec
             VpcSecurityGroupIds = [security_gp],
             )
         if response['ResponseMetadata']['HTTPStatusCode'] == 200:
+            print("***** Successfully create RDS instance *****\n ***** Waiting for RDS instance ****")    
+            waiter = rds.get_waiter('db_instance_available')
+            waiter.wait(DBInstanceIdentifier = db_instance_id)
             exito = 1
-            print("***** Successfully create RDS instance *****\n")    
+            print("***** RDS instance available *****\n")    
         
     except rds.exceptions.DBInstanceAlreadyExistsFault:
         exito = 1
@@ -52,15 +55,15 @@ def create_db_instance(db_instance_id, db_name, db_user, db_pass, subnet_gp, sec
 def db_endpoint(db_instance_id):
     """ Esta funcion devuelve el Endpoint de la base db_name"""
     
-    print("***** waiting for RDS instance to be ready...zzz... *****\n")
+#    print("***** waiting for RDS instance to be ready...zzz... *****\n")
     
     #esperamos a que la instancia este disponible (180 sec e intentamos 5 veces)
     rds = boto3.client('rds', region_name='us-east-1')
-    waiter = rds.get_waiter('db_instance_available')
-    waiter.wait(DBInstanceIdentifier = db_instance_id, 
-                WaiterConfig = {"Delay": 120, "MaxAttempts": 10},
-               )
-    print("***** RDS instance {} ready *****\n".format(db_instance_id))
+#    waiter = rds.get_waiter('db_instance_available')
+#    waiter.wait(DBInstanceIdentifier = db_instance_id)
+#                WaiterConfig = {"Delay": 120, "MaxAttempts": 7},
+               
+#    print("***** RDS instance {} ready *****\n".format(db_instance_id))
     
     dbs = rds.describe_db_instances()
     
@@ -68,7 +71,7 @@ def db_endpoint(db_instance_id):
     for i in range(0, len(dbs.get('DBInstances'))):
         if dbs.get('DBInstances')[i].get('DBInstanceIdentifier') == db_instance_id:
             endpoint = dbs.get('DBInstances')[i].get('Endpoint').get('Address')
-            print('***** RDS instance Endpoint *****\n',endpoint)
+            print('***** RDS instance Endpoint *****\n')
     
     return endpoint
  
@@ -94,7 +97,7 @@ def connect(db_name, db_user, db_pass, db_endpoint):
                                     database = db_name,
                                     password = db_pass,
                                     )
-        print("***** This is the conexion *****\n", connection)
+#        print("***** This is the conexion *****\n", connection)
         return connection
     
     except Exception as error:
@@ -111,6 +114,7 @@ def create_schemas(db_name, db_user, db_pass, db_endpoint):
     connection = connect(db_name, db_user, db_pass, db_endpoint)
     cursor = connection.cursor()
     sql = 'DROP SCHEMA IF EXISTS raw cascade; CREATE SCHEMA raw;'
+    #sql = 'CREATE SCHEMA raw;'
     try:
         cursor.execute(sql)
         connection.commit()
@@ -145,27 +149,25 @@ def create_raw_tables(db_name, db_user, db_pass, db_endpoint):
                                               mes TEXT,
                                               delegacion_cierre TEXT,
                                               fecha_cierre TEXT,
-			                                  mesdecierre TEXT,
+			                      mesdecierre TEXT,
                	                              longitud TEXT,
                                               clas_con_f_alarma TEXT
                                              );
           """)
     
     sql2 = ("""
-            CREATE TABLE raw.metadatos(
-                        dataset TEXT,
-                        timezone TEXT,
-                        rows TEXT,
-                        format TEXT,
-                        refine_ano TEXT,
-                        refine_mes TEXT,
-                        fecha_ejecucion TEXT,
-                        parametros_url TEXT,
-                        ip_address TEXT,
-                        usuario TEXT,
-                        nombre_archivo TEXT,
-                        ruta TEXT
-                        );
+            CREATE TABLE raw.metadatos(dataset TEXT,
+                                       timezone TEXT,
+                                       rows TEXT,
+                                       refine_ano TEXT,
+                                       refine_mes TEXT,
+                                       parametro_url TEXT,
+                                       fecha_ejecucion TEXT,
+                                       ip_address TEXT,
+                                       usuario TEXT,
+                                       nombre_archivo TEXT,
+                                       formato_archivo TEXT
+                                      );
           """)
     
     try:
@@ -173,7 +175,7 @@ def create_raw_tables(db_name, db_user, db_pass, db_endpoint):
         connection.commit()
         cursor.execute(sql2)
         connection.commit()
-        print('***** 2 Tables created*****\n') 
+        print('***** Tables created*****\n') 
     except Exception as error:
         print ("***** Unable to create tables *****\n", error)
     
@@ -182,5 +184,44 @@ def create_raw_tables(db_name, db_user, db_pass, db_endpoint):
 
 
     
-#los nombres de las variables se podrian meter en un archivo csv y hacer referencia a ellas para que no se vea explicito
-#(como lo hizo lilina)
+def bulkInsert(records, meta, db_name, db_user, db_pass, db_endpoint):
+    try:
+        connection = connect(db_name, db_user, db_pass, db_endpoint)
+        cursor = connection.cursor()
+        sql_insert_records = """ INSERT INTO raw.IncidentesViales (latitud, folio,
+                                                                geopoint, hora_creacion,
+                                                                delegacion_inicio, dia_semana,
+                                                                fecha_creacion, ano,
+                                                                tipo_entrada, codigo_cierre,
+                                                                hora_cierre, incidente_c4, 
+                                                                mes, delegacion_cierre, 
+                                                                fecha_cierre, mesdecierre, 
+                                                                longitud, clas_con_f_alarma) 
+                               VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s); """
+
+        sql_insert_metadata = """ INSERT INTO raw.metadatos  (dataset, timezone,
+                                                              rows, refine_ano, 
+                                                              refine_mes, parametro_url,
+                                                              fecha_ejecucion, ip_address, 
+                                                              usuario, nombre_archivo, 
+                                                              formato_archivo)
+
+                                  VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s); """
+
+        # executemany() to insert multiple rows rows
+        cursor.executemany(sql_insert_records, records)
+        connection.commit()
+        print("***** {} Records inserted successfully into table *****".forma(cursor.rowcount))
+        cursor.executemany(sql_insert_metadata, meta)
+        connection.commit()
+        print("***** {} Metadata record inserted successfully *****".format(cursor.rowcount))
+
+    except (Exception, psycopg2.Error) as error:
+        print("***** Failed inserting record into table: {} *****".format(error))
+
+    finally:
+        # closing database connection.
+        if (connection):
+            cursor.close()
+            connection.close()
+            print("***** PostgreSQL connection is closed *****")
