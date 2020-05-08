b2e9676842a391ee7856b7453055e50fe6a6abed
Bcjg23 <54872638+Bcjg23@users.noreply.github.com>
2020-05-08 13:42:04 -0500

    paquetes de modelado

--- script/luigi_files/03_install_requirements.sh
+++ script/luigi_files/03_install_requirements.sh
@@ -3,10 +3,13 @@
 sudo apt update
 
 sudo apt install python3-pip
+sudo apt-get install postgresql-client
 
 pip3 install -r Requirements.txt
+pip3 install sklearn
+pip3 install sklearn_pandas
+pip3 install sqlalchemy
 
-sudo apt-get install postgresql-client
 
 sudo apt install awscli
 
