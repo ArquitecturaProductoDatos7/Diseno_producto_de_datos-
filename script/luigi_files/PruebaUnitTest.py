9eedc7a6b5d02e5a4272f4ef63195af79c16d266
maggiemusa <maggie_musa@hotmail.com>
2020-05-08 21:08:55 -0500

    Primera prueba unitaria con marbles que prueba que la columna incidente_c4_rec tenga 5 categorías

---
+++ script/luigi_files/PruebaUnitTest.py
@@ -0,0 +1,26 @@
+import unittest
+import pandas as pd
+import funciones_s3
+from pandas.util.testing import assert_frame_equal # <-- for testing dataframes
+import marbles.core
+
+class TestFeatureEngineering1(marbles.core.TestCase):
+
+    """ 
+    Clase para probar que el número de categorías en la variable incidente_c4_rec sea 5, 
+    ya que así se reclasificó
+    
+    """
+
+    def setUp(self):
+        
+        """ Your setUp """
+        try:
+            data =funciones_s3.abre_file_como_df('dpa20-incidentes-cdmx', 'bucket_incidentes_cdmx/1.preprocesamiento/base_procesada.csv')
+        except IOError:
+            print ('cannot open file')
+        self.unicos=data['incidente_c4_rec'].nunique()
+        #print(self.unicos)
+
+    def test_colum_Incidente_c4_rec_uniques(self):
+        self.assertEqual(self.unicos, 5, note="El número de categorías de la columna incidente_c4_rec es diferente de 5")
