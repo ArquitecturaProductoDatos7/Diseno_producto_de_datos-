6b4984f027bd0ef89b20c9994d0e28d82e32bbcd
Ubuntu <ubuntu@ip-10-0-0-232.ec2.internal>
2020-05-10 03:46:51 +0000

    pruebas de pruebas unitarias

--- script/luigi_files/PruebaUnitTest3.py
+++ script/luigi_files/PruebaUnitTest3.py
@@ -1,7 +1,7 @@
-import unittest
+#import unittest
 import pandas as pd
 import funciones_s3
-from pandas.util.testing import assert_frame_equal # <-- for testing dataframes
+#from pandas.util.testing import assert_frame_equal # <-- for testing dataframes
 import marbles.core
 
 class TestFeatureEngineering1(marbles.core.TestCase):
@@ -9,21 +9,19 @@ class TestFeatureEngineering1(marbles.core.TestCase):
     """ 
     Clase para probar que el número de categorías en la variable incidente_c4_rec sea 5, 
     ya que así se reclasificó
-    
     """
-    @classmethod
-    def setUpClass(cls):
-        
-        """ Your setUp """
+
+    def __init__(self):
         try:
-            data =funciones_s3.abre_file_como_df('dpa20-incidentes-cdmx', 'bucket_incidentes_cdmx/1.preprocesamiento/base_procesada.csv')
+            self.data =funciones_s3.abre_file_como_df('dpa20-incidentes-cdmx', 'bucket_incidentes_cdmx/1.preprocesamiento/base_procesada.csv')
         except IOError:
             print ('cannot open file', IOError)
-        cls.unicos=data['incidente_c4_rec'].nunique()
-        cls.column1=data['delegacion_inicio']
-    
+        self.unicos = self.data['incidente_c4_rec'].nunique()
+        print(self.unicos)
+        self.column1 = self.data['delegacion_inicio']
+
     def test_colum_Incidente_c4_rec_uniques(self):
         self.assertEqual(self.unicos, 5, note="El número de categorías de la columna incidente_c4_rec es diferente de 5")
-             
+
     def test_islower_w_marbles(self):
         self.assertTrue(self.column1.str.islower().all())
