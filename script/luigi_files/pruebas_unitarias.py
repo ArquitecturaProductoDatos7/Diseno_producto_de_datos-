be2dee8f99e04bf985f7affb8c0aec78796d7aaa
Yalidt <yalidt@hotmail.com>
2020-05-11 18:52:56 -0500

    documenta las pruebas de clean igual que las demas pruebas

--- script/luigi_files/pruebas_unitarias.py
+++ script/luigi_files/pruebas_unitarias.py
@@ -51,26 +51,23 @@ class TestsForExtract(marbles.core.TestCase):
 
 
 class TestClean(marbles.core.TestCase):
+    
     """ 
     Clase para ejecutar las pruebas unitarias en la etapa clean
+    1.- Probar que la columna delegacion_inicio de la base de datos *cleaned* 
+        este en minísculas
+    2.- probar que la columna *mes* de la base de datos *cleaned* 
+        tenga el tipo correcto, en este caso "int.64"
     """
     #Buscamos el archivo del bucket en la S3
     data = funciones_s3.abre_file_como_df('dpa20-incidentes-cdmx', 'bucket_incidentes_cdmx/1.preprocesamiento/base_procesada.csv')
 
     def test_islower_w_marbles(self):
-        """ 
-        Función para probar que la columna delegacion_inicio de la base de datos *cleaned* 
-        este en minísculas
-        """
         column1 = self.data['delegacion_inicio']
 
         self.assertTrue(column1.str.islower().all())
     
     def test_correct_type(self):
-        """ 
-        Función para probar que la columna mes de la base de datos *cleaned* 
-        tenga el tipo correcto
-        """
         mes=self.data['mes']
         
         self.assertTrue(mes.dtype == np.int64)
