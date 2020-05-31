4b491eac7e19f2c48d552b053c12a544caf3fb04
Ubuntu <ubuntu@ip-10-0-0-232.ec2.internal>
2020-05-31 23:41:15 +0000

    actualizado

--- script/luigi_files/03_install_requirements.sh
+++ script/luigi_files/03_install_requirements.sh
@@ -6,13 +6,7 @@ sudo apt install python3-pip
 sudo apt-get install postgresql-client
 
 pip3 install -r Requirements.txt
-pip3 install sklearn
-pip3 install sklearn_pandas
-pip3 install sqlalchemy
-pip3 install marbles
 pip3 install unittest2
-pip install aequitas
-
 
 sudo apt install awscli
 
